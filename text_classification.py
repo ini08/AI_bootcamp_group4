b0dbd9041dc67e9790aacfdf7ad0919e6a02c9d9
ini08 <adedapoinioluwa2@gmail.com>
2023-04-13 18:25:28 +0100

    group4

--- text_classification.py
+++ text_classification.py
@@ -10,10 +10,10 @@ from datetime import datetime
 
 # Utils
 import joblib 
-#pipe_lr = joblib.load(open("models/emotion_classifier_pipe_lr_03_june_2021.pkl","rb"))
+pipe_lr = joblib.load(open("spam_classifier (1).pkl","rb"))
 
 # Fxn
-def predict_emotions(docx):
+def predict(docx):
 	results = pipe_lr.predict([docx])
 	return results[0]
 
@@ -37,32 +37,22 @@ def main():
 			col1,col2  = st.columns(2)
 
 			# Apply Fxn Here
-			prediction = predict_emotions(raw_text)
+			prediction = predict(raw_text)
 			probability = get_prediction_proba(raw_text)
 			
-			add_prediction_details(raw_text,prediction,np.max(probability),datetime.now())
 
 			with col1:
 				st.success("Original Text")
 				st.write(raw_text)
 
 				st.success("Prediction")
-				emoji_icon = emotions_emoji_dict[prediction]
-				st.write("{}:{}".format(prediction,emoji_icon))
-				st.write("Confidence:{}".format(np.max(probability)))
+				st.write("{}".format(prediction))
 
 
 
 			with col2:
 				st.success("Prediction Probability")
-				# st.write(probability)
-				proba_df = pd.DataFrame(probability,columns=pipe_lr.classes_)
-				# st.write(proba_df.T)
-				proba_df_clean = proba_df.T.reset_index()
-				proba_df_clean.columns = ["emotions","probability"]
-
-				fig = alt.Chart(proba_df_clean).mark_bar().encode(x='emotions',y='probability',color='emotions')
-				st.altair_chart(fig,use_container_width=True)
+				st.write(probability)
 
 	else:
 		st.subheader("About")
